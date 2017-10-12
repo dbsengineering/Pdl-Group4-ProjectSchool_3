97fa22573dde9a2cbcddf1e944c50eaa5585380b
DbsEngineering <distro.breizh.stlenn@gmail.com>
2017-10-12 12:41:49 +0200

    Création base de la classe MatrixSWik.java

---
+++ MatrixSynthesizeWikipedia/src/MatrixSWik.java
@@ -0,0 +1,28 @@
+/********************************************************************
+ * 						Classe MatrixSWik							*
+ * 					Départ de l'application							*
+ *																	*
+ *		School : .......... Istic									*
+ *		Formation : ....... Master 1 MIAGE							*
+ *		Lecture : ......... PDL										*
+ *		Group : ........... 4										*
+ *		Project : ......... 3										*
+ *		Authors : ......... Cavron Jérémy, Saker Amine, 			*
+ *							Makroum Siham, Aqasbi Ouahi Mohammed, 	*
+ *							Lachkar Anas, Yassine					*
+ *		DateStart : ....... 12/10/2017								*
+ *		DateModify : ...... 12/10/2017								*
+ *******************************************************************/
+
+
+public class MatrixSWik {
+	
+	/**
+	 * Procédure de démarrage du programme
+	 * @param args : arguments venant du terminal. Type liste de chaînes de caractères.
+	 */
+	public static void main(String[] args){
+		
+	}
+
+}
